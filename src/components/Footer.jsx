a36b8c05ac092e742b2b165736d2f574d5e7dc3c
Rayen Akrich <akricherayen@gmail.com>
2025-07-01 13:13:13 +0100

    restyling the AboutPage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.css';
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <div>© {new Date().getFullYear()} FunPlay. Tous droits réservés.</div>
+    <div>© {new Date().getFullYear()} FunPlay - Rayen Akriche. Tous droits réservés.</div>
   </footer>
 );
 
